4766d175b1f0dabe69e3d3f8bac974492ec5a5a2
sabhya-beginner <agarwalsabhya44@gmail.com>
2024-06-29 20:22:46 +0530

    user & video models

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,7 +3,7 @@
 //package- jwt(json wweb token) --> make tokens
 
 import mongoose, { Schema } from "mongoose";
-import jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken"; //jwt ek bearer token hai--> jiske pas token wo sahi hai
 import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
@@ -53,5 +53,44 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    //string main hi dena hai syntax hai
+    return next();
+  }
+  this.password = bcrypt.hash(this.password, 10); //cannot write callback like ()=>{} bcoz this ka reference nhi pta hota isko
+  next(); //async function likhte hai time lgta hai  10 is hash round
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id, //firstwli paylord se  this waali database se
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id, //firstwli paylord se  this waali database se
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
