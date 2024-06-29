cc9ec400342fddc5b84c84e0bfe18fbea943d882
sabhya-beginner <agarwalsabhya44@gmail.com>
2024-06-29 12:53:46 +0530

    app handling& utils code

--- src/index.js
+++ src/index.js
@@ -8,7 +8,15 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed", err);
+  });
 
 //require("dotenv").config({ path: "./env" });
 /*import express from express;
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,6 @@
+class ApiResponse{
+    constructor(statusCode, data, message="Success"){
+        this.statusCode = statusCode,
+        
+    }
+}
